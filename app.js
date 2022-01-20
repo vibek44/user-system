4d3cd16a738a4631e6a2fce1db4f46a7fd1e843a
vibek44 <kshetrivibek@gmail.com>
2022-01-20 17:14:32 +0200

    initializing the app and add it to server in index

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const path=require('path')
+
+const app=express()
+
+const public=path.join(__dirname, './public')
+
+app.use(express.static(public))
+
+module.exports=app
